422cad68dd40aa4628d9a3f002b5bd4171d9e269
Natalya Shostak <nshostak@klika-tech.com>
2020-10-14 01:35:56 +0300

    Add secure connection

--- client/src/app/components/Ping/index.tsx
+++ client/src/app/components/Ping/index.tsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { loadPing } from '../../actions';
 import { selectPingData, selectPingLoading } from '../../reducers/ping';
 
-const url = 'ws://' + window.location.host + '/ws';
+const url = 'wss://' + window.location.host + '/ws';
 const ws = new WebSocket(url);
 
 ws.onopen = function () {
